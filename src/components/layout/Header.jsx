a3b80e636b8df4554bc3f8212fbfaa88ff07886f
mcpalez <palez.digital@gmail.com>
2024-12-27 00:26:07 +0100

    text changes, structure of apartment html card reorganized

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,8 +2,12 @@ function Header() {
     return (
         <>
             <header className="app-header">
-                <div className="bg-white h-[56px] text-white">
-                    <div className="container mx-auto px-3">Header</div>
+                <div className="bg-white py-2 text-[#04264f]">
+                    <div className="container mx-auto px-3 font-[800] text-[32px]">
+                        <div className="flex items-center">
+                            <div className="logo-app">NOVITO</div>
+                        </div>
+                    </div>
                 </div>
             </header>
         </>
